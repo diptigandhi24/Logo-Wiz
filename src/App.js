3d8c0f6010f40908fa9d815c1879fc8a57741ecb
diptigandhi24 <gandhidipti.s@gmail.com>
2019-06-12 17:06:22 +0530

    gojek till now!!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./data/react.svg";
+import "./App.css";
+
+import data from "./data/logos.json";
+
+//load the svg folder
+var req = require.context("./data", true, /.*\.svg$/);
+let imageData = req.keys().map(function(key) {
+  return req(key);
+});
+imageData.reverse();
 
 function App() {
+  const [imageCount, incrementImageCount] = useState(0); //current position in a array
+  const [currentLogo, changeImage] = useState(imageData[imageCount]);
+  const [userInput, setUserInput] = useState("");
+  // const [winPoint, setWinPoint] = useState(0);
+  let findMatchingString = () => {
+    // console.log(data[imageCount].matches, data[imageCount].matches.length);
+    for (let i = 0; i < data[imageCount].matches.length; i++) {
+      if (userInput == data[imageCount].matches[i]) {
+        console.log("trueeeeeeeeeeeeeee");
+        return true;
+      }
+    }
+  };
+  let winPoint = 0;
+  let onNext = e => {
+    console.log(userInput);
+    console.log(
+      data[imageCount].matches.find(e => (e == userInput ? true : false))
+    );
+
+    if (findMatchingString()) {
+      console.log(data[imageCount].matches, winPoint++);
+    } else {
+      console.log("data doesn't match");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="logoWiz-Wrapper">
+      <p className="logoWiz">Logo Wiz</p>
+      <div className="img-wrapper">
+        <img src={currentLogo} />
+        <input
+          type="text"
+          value={userInput}
+          onChange={e => {
+            setUserInput(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            console.log("Value of ImageCount before", imageCount);
+            incrementImageCount(imageCount + 1);
+            console.log("Value of ImageCount After", imageCount);
+            changeImage(imageData[imageCount]);
+            onNext();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/Logo-Wiz/README.md
@@ -0,0 +1,16 @@
+# Logo Wiz
+
+Make a simple logo quiz app as per the designs.
+
+You’re free to fancy React, Vue, Svelte or any other well known web framework. If you prefer your web apps vanilla (JS) flavored, thats cool too.Your app should:
+* Randomly select 20 logos for one run
+* Maintain the score
+* Match multiple keywords
+
+## Make sure:
+* the app is fast & the UX like hot knife on butter
+* the code is neat, structured well & is a breeze to read
+* is responsive and accessible
+
+## Dazzle us 🤩 :
+* think of one awesomesauce feature of your own like custom ranks, save & load. imagine & make it happen.
